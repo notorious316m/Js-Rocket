f8ff1de9625aa106b87c09d3e3d377b88e5cb21c
Miguel Moraldo <Notorious316m@aim.com>
2019-10-14 20:03:24 -0400

    Implemented Timer Functionality

--- rocket.js
+++ rocket.js
@@ -2,23 +2,52 @@
 var timer = null;
 var countdownNumber = 10;
 
-var changeState = functon (state) {
-  document.body.className = 'body-state'+ 
-  state;
-  clearInterval(timer);
+var changeState = function (state) {
 
-  if (state == 2) {
-  	timer = setInterval(functon () {
-   document.getElementById('countdown').innerHTML = countdownNumber;
-   countdownNumber = countdownNumber-1;
+ document.body.className = 'body-state'+ state;
+ clearInterval(timer);
+ countdownNumber = 10;
 
-   if (countdownNumber <=0) {
-   changeState(3);
-   };
-}, 500);
-} e;se if (state == 3) {
+
+    //countdown
+    if (state == 2) {
+    	timer = setInterval(function () {
+    		document.getElementById('countdown').innerHTML = countdownNumber = countdownNumber -1;
+
+    		if (countdownNumber <= 0) {
+    			changeState(3);
+    	  };
+
+    	}, 500);
+    	
+  
+  } else if (state == 3){
+  	var success = setTimeout (function () 
+  	{
+      var randomNumber = Math.round
+      (Math.random()*10);
+
+      console.log('randomNumber:', randomNumber)
+             
+       // success      
+      if (randomNumber > 5) {
+      	changeState(4);
+
+      }else{
+           changeState(5); // oh no!
+         }
+      
+       }, 2000);
+    };
 
 }
+
+
+
+
+
+
+
 
  
 
